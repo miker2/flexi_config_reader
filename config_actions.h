5bc1aead2a14b4eb13fe0b6da0e0da90548c4527
Michael Rose <michael.rose0@gmail.com>
2021-11-21 00:59:57 +0000

    Begins implementation of include file parsing
    *  A bit of a hack in order to not result in dangling pointers, but it
       works for now.
    *  Need to figure out how to solve the input issue.
    *  Code needs much cleanup.

--- config_actions.h
+++ config_actions.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <filesystem>
+#include <iostream>
 #include <tao/pegtl.hpp>
 #include <tao/pegtl/contrib/parse_tree.hpp>
 
@@ -10,16 +12,65 @@ namespace config {
 // TODO: Strip trailing whitespace from comments!
 
 template <typename Rule>
-using selector = peg::parse_tree::selector<
-    Rule,
-    peg::parse_tree::store_content::on<HEX, NUMBER, STRING, VAR, FLAT_KEY, KEY, COMMENT,
-                                       filename::FILENAME>,
-    peg::parse_tree::remove_content::on<END, VARADD, VARREF, INCLUDE>,
-    /*
-      peg::parse_tree::remove_content::on<WS_, NL, SP, oSP, COMMENT, TAIL,
-      COMMA, SBo, SBc, CBo, CBc, KVs, HEXTAG, sign, exp>,
-    */
-    peg::parse_tree::fold_one::on<CONFIG, STRUCTc, STRUCT, PROTO, REFERENCE, VALUE, PAIR, LIST>>;
+struct selector : peg::parse_tree::selector<
+                      Rule,
+                      peg::parse_tree::store_content::on<HEX, NUMBER, STRING, VAR, FLAT_KEY, KEY,
+                                                         COMMENT, filename::FILENAME>,
+                      peg::parse_tree::remove_content::on<END, VARADD, VARREF>,
+                      peg::parse_tree::fold_one::on<CONFIG, STRUCTc, STRUCT, PROTO, REFERENCE,
+                                                    VALUE, PAIR, LIST>> {};
+
+template <>
+struct selector<INCLUDE> : std::true_type {
+  template <typename... States>
+  static void transform(std::unique_ptr<peg::parse_tree::node>& n, States&&... st) {
+    std::cout << "Successfully found a " << n->type << " node in " << n->source << "!\n";
+    std::string filename = "";
+    for (const auto& c : n->children) {
+      std::cout << "Has child of type " << c->type << ".\n";
+      if (c->has_content()) {
+        std::cout << "  Content: " << c->string_view() << "\n";
+      }
+      if (c->is_type<filename::FILENAME>()) {
+        filename = c->string();
+      }
+    }
+    std::cout << std::endl;
+
+    std::cout << " --- Node has " << n->children.size() << " children.\n";
+
+    //// Let's try to parse the "include" file and replace this node with the output!
+    // Build the parse path:
+    auto include_path = std::filesystem::path(n->source).parent_path() / filename;
+
+    std::cout << "Trying to parse: " << include_path << std::endl;
+    auto input_file = peg::file_input(include_path);
+
+    // This is a bit of a hack. We need to use a 'string_input' here because if we don't, the
+    // 'file_input' will go out of scope, and the generating the 'dot' output will fail. This would
+    // likely be the case when trying to walk the parse tree later.
+    // TODO: Find a more elegant way of
+    // doing this.
+    const auto file_contents = std::string(input_file.begin(), input_file.size());
+    auto input = peg::string_input(file_contents, input_file.source());
+
+    if (auto new_tree = peg::parse_tree::parse<config::grammar, config::selector>(input)) {
+      peg::parse_tree::print_dot(std::cout, *new_tree->children.back());
+      auto c = std::move(new_tree->children.back());
+      std::cout << " --- Parsed include file has " << c->children.size() << " children.\n";
+      new_tree->children.pop_back();
+      n->remove_content();
+      n = std::move(c);
+      std::cout << "c=" << c << std::endl;
+      std::cout << "n=" << n << std::endl;
+      std::cout << " --- n has " << n->children.size() << " children." << std::endl;
+      std::cout << " --- Source: " << n->source << std::endl;
+      std::cout << " ------ size: " << n->source.size() << std::endl;
+    } else {
+      std::cout << "Failed to properly parse " << include_path << "." << std::endl;
+    }
+  }
+};
 
 template <typename Rule>
 struct action : peg::nothing<Rule> {};
