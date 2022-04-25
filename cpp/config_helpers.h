a29b0eba143fd1b7d464f44e1b091910ceee88fa
Michael Rose <michael.rose0@gmail.com>
2022-04-25 22:43:36 +0100

    Makes a number of methods const
    *  Moves a function out of the ConfigReader into a namespace

--- cpp/config_helpers.h
+++ cpp/config_helpers.h
@@ -306,6 +306,48 @@ inline auto getConfigValue(const config::types::CfgMap& cfg,
   return cfg.at(var->keys.back());
 }
 
+inline void resolveVarRefs(const config::types::CfgMap& root,
+                           config::types::CfgMap& sub_tree, const std::string& parent_key = "") {
+  for (const auto& kv : sub_tree) {
+    const auto src_key = utils::makeName(parent_key, kv.first);
+    if (kv.second->type == config::types::Type::kValueLookup) {
+      // Add the source key to the reference list (if we ever get back to this key, it's a failure).
+      std::vector<std::string> refs = {src_key};
+      logger::trace("For {}, found {} (type={}).", src_key, kv.second, kv.second->type);
+      // Initialize 'value' for use within the loop.
+      auto value = kv.second;
+      // Follow the kValueLookup objects until:
+      //  (a) A terminal value is found, upon which the value is used.
+      //  (b) A cycle is found, upon which an exception is thrown.
+      do {
+        auto kv_lookup = dynamic_pointer_cast<config::types::ConfigValueLookup>(value);
+        if (utils::contains(refs, kv_lookup->var())) {
+          // This key has already been traversed/referenced. A cycle has been found!
+          THROW_EXCEPTION(config::CyclicReferenceException,
+                          "For {}, found a cyclic reference when trying to resolve {}.\n  "
+                          "Reference chain: [{}]\n",
+                          src_key, kv.second, fmt::join(refs, " -> "));
+        }
+        // Get the new value based on the kValueLookup object.
+        value = config::helpers::getConfigValue(root, kv_lookup);
+        logger::trace("{} points to {}", static_pointer_cast<config::types::ConfigBase>(kv_lookup),
+                      value);
+        // Add this key to the list of references/dependencies
+        refs.emplace_back(kv_lookup->var());
+        logger::trace("refs: [{}]", fmt::join(refs, " -> "));
+      } while (value->type == config::types::Type::kValueLookup);
+      logger::debug("{} contains instance of ValueLookup: {}. Has value={}", src_key, kv.second,
+                    value);
+
+      // If we've gotten here, then we can set the value of this key!
+      sub_tree[kv.first] = value;
+    } else if (config::helpers::isStructLike(kv.second)) {
+      resolveVarRefs(root, dynamic_pointer_cast<config::types::ConfigStructLike>(kv.second)->data,
+                     src_key);
+    }
+  }
+}
+
 inline auto unflatten(const std::span<std::string> keys, const config::types::CfgMap& cfg)
     -> config::types::CfgMap {
   if (keys.empty()) {
