c26e0230e08ddf0fcd948b61375e80707a6d634c
Michael Rose <michael.rose0@gmail.com>
2022-05-01 20:54:43 +0100

    Adds handling for VAR_REF objects in math grammar
    *  When a `VAR_REF` is encountered while parsing a math grammar,
       look up the value and use it instead of the VAR_REF.
    *  Adds tests for both successful and failing cases.

--- tests/math_test.cpp
+++ tests/math_test.cpp
@@ -6,6 +6,7 @@
 #include <tao/pegtl/contrib/analyze.hpp>
 #include <tao/pegtl/contrib/parse_tree.hpp>
 #include <tao/pegtl/contrib/parse_tree_to_dot.hpp>
+#include <tuple>
 #include <unordered_map>
 #include <utility>
 #include <vector>
@@ -26,14 +27,20 @@ const std::vector<std::pair<std::string, double>> test_strings = {
     {"-4.7 * -(3.72 + -pi  ) ", 2.7185145281279732},
     {"  1/3 * -( 5 + 4 )  ", -3.0},
     {"\t3.4 * -(1.9**2 * (1/3.1 - 6) * (2.54- 17.0)\t)", -1007.6399690322581}};
-}
+
+using RefMap = std::map<std::string, double>;
+
+const std::vector<std::tuple<std::string, double, RefMap>> test_w_var_ref = {
+    {"0.5 * ($(test1.key) - 0.234)", 0.503, {{"test1.key", 1.24}}},
+    {"3*$(var_ref1) - 2.3**$(exponent) - 5 * 4",
+     -20.70657508881031,
+     {{"var_ref1", 0.27}, {"exponent", 0.5}}},
+    {"  $(its.a.three) ^ 2.4 * $(another.var) + $(one.more.value) + 4.3 ",
+     174.79264401590646,
+     {{"its.a.three", 3}, {"another.var", 12.2}, {"one.more.value", 0.1}}}};
+}  // namespace
 
 TEST(math_expression, analyze) { ASSERT_EQ(peg::analyze<math::expression>(), 0); }
-
-TEST(math_expression, grammar2_analyze) {
-  namespace math = grammar2;
-  ASSERT_EQ(peg::analyze<math::expression>(), 0);
-}
 
 TEST(math_expression, evaluate) {
   auto test_input = [](const std::string& input) -> double {
@@ -49,9 +56,37 @@ TEST(math_expression, evaluate) {
     EXPECT_NO_THROW(result = test_input(input.first));
     EXPECT_FLOAT_EQ(result, input.second);
   }
+
+  // We'll intentionally omit the var_ref_map here to ensure a failure occurs
+  for (const auto& input : test_w_var_ref) {
+    std::cout << "Input: " << std::get<0>(input) << std::endl;
+    EXPECT_THROW(test_input(std::get<0>(input)), std::runtime_error);
+  }
 }
 
-TEST(math_expression, grammar2_evaluate) {
+TEST(math_expression, evaluate_var_ref) {
+  auto test_input = [](const std::string& input, const RefMap& ref_map) -> double {
+    peg::memory_input in(input, "from content");
+    math::ActionData out;
+    out.var_ref_map = ref_map;
+    const auto result = peg::parse<math::expression, math::action>(in, out);
+    return out.res;
+  };
+
+  for (const auto& input : test_w_var_ref) {
+    std::cout << "Input: " << std::get<0>(input) << std::endl;
+    double result{0};
+    EXPECT_NO_THROW(result = test_input(std::get<0>(input), std::get<2>(input)));
+    EXPECT_FLOAT_EQ(result, std::get<1>(input));
+  }
+}
+
+TEST(grammar2_expression, analyze) {
+  namespace math = grammar2;
+  ASSERT_EQ(peg::analyze<math::expression>(), 0);
+}
+
+TEST(grammar2_expression, evaluate) {
   namespace math = grammar2;
 
   auto test_input = [](const std::string& input) -> double {
@@ -66,5 +101,30 @@ TEST(math_expression, grammar2_evaluate) {
     double result{0};
     EXPECT_NO_THROW(result = test_input(input.first));
     EXPECT_FLOAT_EQ(result, input.second);
+  }
+
+  // We'll intentionally omit the var_ref_map here to ensure a failure occurs
+  for (const auto& input : test_w_var_ref) {
+    std::cout << "Input: " << std::get<0>(input) << std::endl;
+    EXPECT_THROW(test_input(std::get<0>(input)), std::runtime_error);
+  }
+}
+
+TEST(grammar2_expression, evaluate_var_ref) {
+  namespace math = grammar2;
+
+  auto test_input = [](const std::string& input, const RefMap& ref_map) -> double {
+    peg::memory_input in(input, "from content");
+    ::math::ActionData out;
+    out.var_ref_map = ref_map;
+    const auto result = peg::parse<math::expression, math::action>(in, out);
+    return out.res;
+  };
+
+  for (const auto& input : test_w_var_ref) {
+    std::cout << "Input: " << std::get<0>(input) << std::endl;
+    double result{0};
+    EXPECT_NO_THROW(result = test_input(std::get<0>(input), std::get<2>(input)));
+    EXPECT_FLOAT_EQ(result, std::get<1>(input));
   }
 }
