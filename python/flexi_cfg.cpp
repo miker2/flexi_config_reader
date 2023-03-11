ca9c3ea3943b71cf54a20aefd0aaecb795cf8433
Michael Rose <michael.rose0@gmail.com>
2023-03-11 09:21:10 -0800

    C++ version: Adds reader for unsigned types Fixes bug with reading hex types
    Python version:
    Adds getters for int, float, & string types

--- python/flexi_cfg.cpp
+++ python/flexi_cfg.cpp
@@ -1,4 +1,5 @@
 #include <pybind11/pybind11.h>
+#include <pybind11/stl.h>
 
 #include <flexi_cfg/parser.h>
 #include <flexi_cfg/reader.h>
@@ -14,10 +15,20 @@ py::class_<std::filesystem::path>(m, "Path")
 py::implicitly_convertible<std::string, std::filesystem::path>();
 */
 
+template <typename T>
+auto getValueHelper(const flexi_cfg::Reader& cfg, const std::string& key) -> T {
+    return cfg.getValue<T>(key);
+}
 
 PYBIND11_MODULE(py_flexi_cfg, m) {
     py::class_<flexi_cfg::Reader>(m, "Reader")
-        .def("dump", &flexi_cfg::Reader::dump);
+        .def("dump", &flexi_cfg::Reader::dump)
+        .def("exists", &flexi_cfg::Reader::exists)
+        .def("keys", &flexi_cfg::Reader::keys)
+        .def("find_struc_with_key", &flexi_cfg::Reader::findStructsWithKey)
+        .def("get_int", &getValueHelper<int>)
+        .def("get_float", &getValueHelper<double>)
+        .def("get_string", &getValueHelper<std::string>);
 
     py::class_<flexi_cfg::Parser>(m, "Parser")
         .def("parse", py::overload_cast<std::string_view, std::string_view>(
