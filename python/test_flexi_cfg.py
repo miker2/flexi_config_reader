e76047eb7604aa16e21aedd4108947453288667d
Michael Rose <michael.rose@figure.ai>
2023-04-11 16:39:50 -0700

    Generate python stub file (#111)
    * Cleans up python bindings so a stub file can be generated
    * Adds a generic 'getValue' method that supports python ducktyping
    * Note: stubs won't be generated for Darwin

--- python/test_flexi_cfg.py
+++ python/test_flexi_cfg.py
@@ -45,12 +45,12 @@ class TestMyConfig(unittest.TestCase):
                                   'var_ref': 10},
                         'my_list': [1.0, -2, 4.2]}
         cfg = flexi_cfg.parse(my_config_example, "example_cfg")
-        self.assertEqual(cfg.getValue_string("test1.key1"), expected_cfg["test1"]["key1"])
-        self.assertEqual(cfg.getValue_double("test1.key2"), expected_cfg["test1"]["key2"])
-        self.assertEqual(cfg.getValue_int("test1.key3"), expected_cfg["test1"]["key3"])
-        self.assertEqual(cfg.getValue_uint64("test2.my_hex"), expected_cfg["test2"]["my_hex"])
-        self.assertEqual(cfg.getValue_bool("test2.bool_key"), expected_cfg["test2"]["bool_key"])
-        self.assertEqual(cfg.getValue_list_double("my_list"), expected_cfg['my_list'])
+        self.assertEqual(cfg.getString("test1.key1"), expected_cfg["test1"]["key1"])
+        self.assertEqual(cfg.getFloat("test1.key2"), expected_cfg["test1"]["key2"])
+        self.assertEqual(cfg.getInt("test1.key3"), expected_cfg["test1"]["key3"])
+        self.assertEqual(cfg.getUint64("test2.my_hex"), expected_cfg["test2"]["my_hex"])
+        self.assertEqual(cfg.getBool("test2.bool_key"), expected_cfg["test2"]["bool_key"])
+        self.assertEqual(cfg.getFloatList("my_list"), expected_cfg['my_list'])
 
         self.assertEqual(sorted(cfg.keys()), sorted(expected_cfg.keys()))
         self.assertEqual(cfg.getType("test1.key1"), flexi_cfg.Type.kString)
@@ -60,7 +60,7 @@ class TestMyConfig(unittest.TestCase):
         self.assertEqual(cfg.getType("test2.bool_key"), flexi_cfg.Type.kBoolean)
         self.assertEqual(cfg.getType("my_list"), flexi_cfg.Type.kList)
 
-        cfg_test2 = cfg.getValue_reader("test2")
+        cfg_test2 = cfg.getReader("test2")
         self.assertEqual(sorted(cfg_test2.keys()), sorted(expected_cfg["test2"].keys()))
         
     def test_cfg_file(self):
@@ -84,11 +84,11 @@ class TestMyConfig(unittest.TestCase):
         cfg = flexi_cfg.parse(cfg_file_path)
 
         self.assertEqual(sorted(cfg.keys()), sorted(expected_cfg.keys()))
-        self.assertEqual(cfg.getValue_double('test1.key3'), cfg.getValue_double('test2.var_ref'))
-        self.assertAlmostEqual(cfg.getValue_double('test1.key3'), expected_cfg['test1']['key3'], places=6)
+        self.assertEqual(cfg.getFloat('test1.key3'), cfg.getFloat('test2.var_ref'))
+        self.assertAlmostEqual(cfg.getFloat('test1.key3'), expected_cfg['test1']['key3'], places=6)
 
         self.assertEqual(cfg.getType('test1.f'), flexi_cfg.Type.kList)
-        self.assertEqual(cfg.getValue_list_string('test1.f'), expected_cfg['test1']['f'])
+        self.assertEqual(cfg.getStringList('test1.f'), expected_cfg['test1']['f'])
         
 
 if __name__ == '__main__':
