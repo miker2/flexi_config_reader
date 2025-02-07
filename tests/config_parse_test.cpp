e656342c20f6bf2d8c56d9bf6f2ff18623f99678
Michael Rose <michael.rose0@gmail.com>
2025-02-07 14:57:14 -0800

    fix clang-tidy

--- tests/config_parse_test.cpp
+++ tests/config_parse_test.cpp
@@ -95,7 +95,7 @@ TEST_P(InputString, Reader) {
   // }
   EXPECT_TRUE(cfg.exists("test2.inner.expression"));
   EXPECT_EQ(cfg.getType("test2.inner.expression"), flexi_cfg::config::types::Type::kNumber);
-  EXPECT_FLOAT_EQ(cfg.getValue<float>("test2.inner.expression"), 1.f / sqrt(2.0f));
+  EXPECT_FLOAT_EQ(cfg.getValue<float>("test2.inner.expression"), 1.F / sqrt(2.0F));
 
   // Coverage for override. The values below should match the value on the following line:
   //  a [override] = 2
